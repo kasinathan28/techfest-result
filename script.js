5d1f88a8de1664a765f2d438cb69574629fc2a7a
cptpro00 <way2kasinathb35@gmail.com>
2024-01-02 00:53:57 +0530

    latest 3

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ var x = setInterval(function() {
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the countdown
-    document.getElementById("countdown").innerHTML = days + "d : " + hours + "h : " + minutes + "m : " + seconds + "s ";
-
+    document.getElementById("countdown").innerHTML =
+    days + "d : " + hours + "h : " + minutes + "m : <span class='seconds'>" + seconds + "s</span>";
+  
     // If the countdown is over, display a message
     if (distance < 0) {
         clearInterval(x);
