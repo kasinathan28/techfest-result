04068a51ce3c0cfea44b366cfbd1e4623b003cf5
Kasinathan <68454568+kasinathan28@users.noreply.github.com>
2024-01-02 17:43:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     document.getElementById("countdown").innerHTML =
     days + "d : " + hours + "h : " + minutes + "m : <span class='seconds'>" + seconds + "s</span>";
-  // If the countdown is over, display a message
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "EXPIRED";
