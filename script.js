ed43b6afaee70d7b41751b69a580316cd0e86adf
Kasinathan <68454568+kasinathan28@users.noreply.github.com>
2024-01-02 17:43:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
 
-// Set the date we're counting down to
 var countDownDate = new Date("Jan 25, 2024 00:00:00").getTime();
-
-// Update the countdown every 1 second
 var x = setInterval(function() {
-
-    // Get the current date and time
     var now = new Date().getTime();
-
-    // Calculate the remaining time
     var distance = countDownDate - now;
-
-    // Calculate days, hours, minutes, and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
     document.getElementById("countdown").innerHTML =
     days + "d : " + hours + "h : " + minutes + "m : <span class='seconds'>" + seconds + "s</span>";
-  
-    // If the countdown is over, display a message
+  // If the countdown is over, display a message
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "EXPIRED";
